8d6243a680f5252e4a028be019b07ba18175e8a0
mogere <pmnyatichi@gmail.com>
2020-03-31 13:35:59 +0300

    add locations and their flags

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:worldtime/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
 
 class Loading extends StatefulWidget {
   @override
@@ -6,10 +9,38 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+
+
+  void setupWorldTime() async{
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime,
+    });
+
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child:SpinKitRing(
+          color: Colors.white,
+          size: 80.0,
+        ),
+      )
     );
   }
 }
